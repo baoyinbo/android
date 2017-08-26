f84be672d23d935f1d754c6bc5009a4d576b520f
baoyb <baoyinbo@163.com>
2017-08-26 12:09:28 +0800

    test

--- Vidio/app/src/main/java/com/byb/vidio/MainActivity.java
+++ Vidio/app/src/main/java/com/byb/vidio/MainActivity.java
@@ -3,12 +3,15 @@ package com.byb.vidio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.byb.lazynetlibrary.logger.LazyLogger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LazyLogger.i("tets");
 
     }
 }
